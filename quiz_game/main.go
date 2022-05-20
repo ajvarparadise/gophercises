013aaa1f2c7b8c69085bca2be88d6284ad9b7a6d
Admir Husovic <mail.admir.husovic@gmail.com>
2022-05-20 11:39:14 +0200

    basic game logic

---
+++ quiz_game/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func loadQuiz() {
+	//parse the csv with any package
+}
+func main() {
+	// start game
+	// did user provide flag for own file to parse? else use problems.csv in same dir as program
+	// loadQuiz
+	// present first question to user
+	// compare anwser ? correct / incorrect
+	// present next question
+	// is last question? end game
+	fmt.Println("Hello, 世界")
+}
