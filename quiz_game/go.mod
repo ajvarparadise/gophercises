0298b8337ab3b623ac536e9d1f2335d9257323ad
Admir Husovic <mail.admir.husovic@gmail.com>
2022-05-26 07:45:13 +0200

    wip: ask question loop

---
+++ quiz_game/go.mod
@@ -0,0 +1,3 @@
+module github.com/ajvarparadise/gophercises/quiz_game
+
+go 1.18
--- quiz_game/main.go
+++ quiz_game/main.go
@@ -1,17 +1,54 @@
 package main
 
-import "fmt"
+// start game
+// did user provide flag for own file to parse? else use problems.csv in same dir as program
+// loadQuiz
+// present first question to user
+// compare anwser ? correct / incorrect
+// present next question
+// is last question? end game
 
-func loadQuiz() {
-	//parse the csv with any package
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func readFile() *csv.Reader {
+	// set args for examples sake
+	f, err := os.Open("problems.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	r := csv.NewReader(f)
+
+	return r
 }
+func askQuestions() {
+	// read one row at a time
+	r := readFile()
+	for {
+		problem, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		question := problem[0]
+		answer := problem[1]
+
+		var quess string
+		// Taking input from user
+		fmt.Printf("what is %s \n", question)
+		fmt.Println("answer: ")
+		fmt.Scanln(&quess)
+		fmt.Printf("Question: %s -> answer: %v -> quess: %v\n", question, answer, quess)
+	}
+}
+
 func main() {
-	// start game
-	// did user provide flag for own file to parse? else use problems.csv in same dir as program
-	// loadQuiz
-	// present first question to user
-	// compare anwser ? correct / incorrect
-	// present next question
-	// is last question? end game
-	fmt.Println("Hello, 世界")
+	askQuestions()
 }
