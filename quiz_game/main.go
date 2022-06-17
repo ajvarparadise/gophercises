4864673d8261feecf1709350d3ab16be948db738
Admir Husovic <mail.admir.husovic@gmail.com>
2022-06-17 11:49:01 +0200

    wip

--- quiz_game/main.go
+++ quiz_game/main.go
@@ -15,17 +15,19 @@ import (
 	"io"
 	"log"
 	"os"
+	"time"
 )
 
 var filename string
+var timelimit int
 
 func init() {
 	flag.StringVar(&filename, "filename", "./problems.csv", "define relative path to custom csv questionare file")
+	flag.IntVar(&timelimit, "timelimit", 30000, "define time limit")
 	flag.Parse()
 }
 
 func readFile() *csv.Reader {
-	// set args for examples sake
 	f, err := os.Open(filename)
 	if err != nil {
 		log.Fatal(err)
@@ -34,28 +36,49 @@ func readFile() *csv.Reader {
 
 	return r
 }
+
+func getNextQuestion(r *csv.Reader) (string, string, error) {
+	problem, err := r.Read()
+	if err == io.EOF {
+		return "", "", err
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	question := problem[0]
+	answer := problem[1]
+	return question, answer, err
+}
+
 func askQuestions() {
 	// read one row at a time
-	r := readFile()
 	var correctQuesses = 0
 	var incorrectQuess = 0
+	var start string
+	var quess string
+	r := readFile()
 
+	fmt.Printf("Hi there, press enter to start the game: \n")
+	fmt.Scanln(&start)
 	for {
-		problem, err := r.Read()
-		if err == io.EOF {
-			break
-		}
+		question, answer, err := getNextQuestion(r)
 		if err != nil {
-			log.Fatal(err)
+			os.Exit(0)
 		}
-		question := problem[0]
-		answer := problem[1]
-
-		var quess string
-		// Taking input from user
 		fmt.Printf("question: %s \n", question)
 		fmt.Println("answer: ")
+
+		gameTimer := time.NewTimer(3000 * time.Millisecond)
+		go func() {
+			println("go func")
+			<-gameTimer.C
+			gameTimer.Stop()
+			fmt.Println("Time expired")
+			fmt.Printf("correct: %d, incorrect: %d \n\n", correctQuesses, incorrectQuess)
+			os.Exit(0)
+		}()
 		fmt.Scanln(&quess)
+		gameTimer.Reset(3000 * time.Millisecond)
 		if quess == answer {
 			correctQuesses++
 		} else {
@@ -63,7 +86,6 @@ func askQuestions() {
 		}
 		fmt.Println()
 	}
-	fmt.Printf("correct: %d, incorrect: %d \n\n", correctQuesses, incorrectQuess)
 }
 
 func main() {
